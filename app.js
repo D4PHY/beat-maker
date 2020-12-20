0ebbe04e124ddac895fbdd884d9389667159132a
dAFI <dafinadavid@gmail.com>
2020-12-20 11:05:37 +0200

    implemented play and stop

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-class BeatMaker {
+class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.btnPlay = document.querySelector(".btn-play");
     this.index = 0;
+    this.bpm = 60;
+    this.soundLoop;
   }
-  soundLoop() {
-    let speed = 300;
-    this.pads.forEach((pad) => {
-      this.pads.forEach((pad) => {
-        if (pad.classList.contains(`b${this.index}`)) {
-          setInterval(() => {
-            setTimeout(() => {
-              console.log(pad, this.index);
-              if (pad.style.transform === "scale(1)") {
-                pad.style.transform = "scale(1.2)";
-              } else {
-                pad.style.transform = "scale(1)";
-              }
-            }, this.index * speed);
-          }, speed * 8);
-        }
-      });
-      this.index++;
-    });
+
+  repeat() {
+    let step = this.index % 8;
+    const activePads = document.querySelectorAll(`.b${step}`);
+    console.log(activePads);
+    this.index++;
+  }
+
+  start() {
+    this.btnPlay.textContent = "Stop";
+    const interval = (60 / this.bpm) * 1000;
+    this.soundLoop = setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+
+  stop() {
+    clearInterval(this.soundLoop);
+    this.btnPlay.textContent = "Play";
   }
 }
 
-const beatMaker = new BeatMaker();
-beatMaker.soundLoop();
+const drumKit = new DrumKit();
+drumKit.btnPlay.addEventListener("click", () => {
+  if (drumKit.btnPlay.textContent === "Play") {
+    drumKit.start();
+  } else {
+    drumKit.stop();
+  }
+});
+// drumKit.start();
